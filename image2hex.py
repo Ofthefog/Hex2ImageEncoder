63c9718eee88acfd55633090615f7e13f009b9d4
Ofthefog <14334907+Ofthefog@users.noreply.github.com>
2020-04-05 20:23:52 +0200

    clean irrelevant comments

--- image2hex.py
+++ image2hex.py
@@ -30,14 +30,3 @@ for y in range(dim):
 	
 	hout.write(hexstripped)
 	
-
-
-"""
-im = Image.open('dead_parrot.jpg') # Can be many different formats.
-pix = im.load()
-print im.size  # Get the width and hight of the image for iterating over
-print pix[x,y]  # Get the RGBA Value of the a pixel of an image
-pix[x,y] = value  # Set the RGBA Value of the image (tuple)
-im.save('alive_parrot.png')  # Save the modified pixels as .png
-
-"""
