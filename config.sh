b5add7b4bd7518dbb65c63035eff117fe103b152
Ofthefog <14334907+Ofthefog@users.noreply.github.com>
2020-04-15 21:07:15 +0200

    Add config file

---
+++ config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Will we skip confirmation when a file already exists?
+# Default is false
+overwrite=false
+
+# Will the intermediary files be deleted after outputting the image?
+# Default is true
+cleanup=true
